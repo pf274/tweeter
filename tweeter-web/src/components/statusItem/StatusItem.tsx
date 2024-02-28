28afff2f44ea1270c18f80dc7e0b46e53c619c93
pf274 <peterfullmer33@gmail.com>
2024-02-28 16:01:24 -0700

    Abstracted Item Scroller

--- tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -3,52 +3,40 @@ import { Status } from "tweeter-shared";
 import Post from "./Post";
 import useUserNavigation from "../userInfo/UserNavigationHook";
 
-
 interface Props {
-  status: Status
+  value: Status;
 }
 
-
-function StatusItem({status}: Props) {
+function StatusItem({ value }: Props) {
   const { navigateToUser } = useUserNavigation();
 
   return (
-      <div
-        className="row mb-3 mx-0 px-0 border rounded bg-white"
-      >
-        <div className="col bg-light mx-0 px-0">
-          <div className="container px-0">
-            <div className="row mx-0 px-0">
-              <div className="col-auto p-3">
-                <img
-                  src={status.user.imageUrl}
-                  className="img-fluid"
-                  width="80"
-                  alt="Posting user"
-                />
-              </div>
-              <div className="col">
-                <h2>
-                  <b>
-                    {status.user.firstName} {status.user.lastName}
-                  </b>{" "}
-                  -{" "}
-                  <Link
-                    to={status.user.alias}
-                    onClick={(event) => navigateToUser(event)}
-                  >
-                    {status.user.alias}
-                  </Link>
-                </h2>
-                {status.formattedDate}
-                <br />
-                <Post status={status} />
-              </div>
+    <div className="row mb-3 mx-0 px-0 border rounded bg-white">
+      <div className="col bg-light mx-0 px-0">
+        <div className="container px-0">
+          <div className="row mx-0 px-0">
+            <div className="col-auto p-3">
+              <img src={value.user.imageUrl} className="img-fluid" width="80" alt="Posting user" />
+            </div>
+            <div className="col">
+              <h2>
+                <b>
+                  {value.user.firstName} {value.user.lastName}
+                </b>{" "}
+                -{" "}
+                <Link to={value.user.alias} onClick={(event) => navigateToUser(event)}>
+                  {value.user.alias}
+                </Link>
+              </h2>
+              {value.formattedDate}
+              <br />
+              <Post status={value} />
             </div>
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
 export default StatusItem;
